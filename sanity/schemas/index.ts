44b31225ca56c005aab7887c12aa3c1e7087e5d9
prikol769 <prikol769@gmail.com>
2023-09-27 10:48:31 -0500

    added resource schemas

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,9 @@
+import resource from './resource.schema';
+import resourcePlaylist from './resource-playlist.schema';
+
+const schemas = [
+    resource,
+    resourcePlaylist
+]
+
+export default schemas;
